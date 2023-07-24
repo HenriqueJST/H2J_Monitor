066a1c6784860c35ea06f9f6c915c44901c63819
HenriqueJST <henriquestieve.senai@gmail.com>
2023-07-24 13:22:56 -0300

    Atualizado o software de instalação

--- Update.bat
+++ Update.bat
@@ -1,9 +1,6 @@
 @echo off
-set "fileName=main.exe"
-set "fileURL=https://drive.google.com/file/d/1uEbRUeZpXXL2tIMSncsyMgEHorRUmMtF/view"
-
-@REM net stop "H2J Monitor"
-@REM ping 127.0.0.1 -n 3 > nul
+net stop "H2J Monitor"
+ping 127.0.0.1 -n 3 > nul
 cd /d "%~dp0"
-curl -L -o "https://drive.google.com/file/d/1uEbRUeZpXXL2tIMSncsyMgEHorRUmMtF/view?usp=sharing" "main.exe"
-@REM net start "H2J Monitor"
+curl -LJO https://github.com/HenriqueJST/H2J_Monitor/raw/main/src/main.exe
+net start "H2J Monitor"
--- install.bat
+++ install.bat
@@ -12,8 +12,9 @@ set "DEFAULT_QLIK_LOGS_PATH_ONPRIMESE=C:/ProgramData/Qlik/Sense/Log"
 set "DEFAULT_ENGINEPORT=4747"
 set "DEFAULT_CERTIFICATESPATH=C:/ProgramData/Qlik/Sense/Repository/Exported Certificates/.Local Certificates"
 
-set /p ACTION="Deseja Instalar (I) ou Desinstalar (D)? (I/D): "
+set /p ACTION="Deseja Instalar (I), Desinstalar (D) ou dar Update (U)? (I/D/U): "
 if /i "%ACTION%"=="I" (
+    curl -LJO https://github.com/HenriqueJST/H2J_Monitor/raw/main/src/main.exe
     cls
     main.exe -mode=install
     cls
@@ -78,14 +79,27 @@ if /i "%ACTION%"=="I" (
     echo ==============================================
     echo Pressione qualquer tecla para sair.    
     pause > nul
-) else (
-    if /i "%ACTION%"=="D" (
+) else if /i "%ACTION%"=="D" (
         cd /d "%~dp0"
         echo Desinstalando......
         main.exe -mode=uninstall
         echo Pressione qualquer tecla para sair.
         pause > nul
-    ) else (
-        exit /b
-    )
+   ) else if /i "%ACTION%"=="U" (
+    net stop "H2J Monitor"
+    cls
+    ping 127.0.0.1 -n 3 > nul
+    cls
+    cd /d "%~dp0"
+    curl -LJO https://github.com/HenriqueJST/H2J_Monitor/raw/main/src/main.exe
+    net start "H2J Monitor"
+    cls
+    echo ==============================================
+    echo             Update Successfully
+    echo ==============================================
+    echo Pressione qualquer tecla para sair.    
+    pause > nul
+) else (
+    echo Opção inválida. Por favor, escolha I, D ou U.
 )
+
